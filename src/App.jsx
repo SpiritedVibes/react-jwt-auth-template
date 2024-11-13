8e70156c346e36d961e0e2621aee7572b6ec37c9
SpiritedVibes <austinrassman@gmail.com>
2024-11-13 14:13:12 -0500

    signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-// src/App.jsx
-
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar'
+import Landing from './components/Landing/Landing'
+import Dashboard from './components/Dashboard/Dashboard'
+import SignupForm from './components/SignupForm/SignupForm' // import the SignupForm
 
 const App = () => {
   const [user, setUser] = useState(null)
+  
+return (
+  <>
+    <NavBar user={user} />
+    <Routes>
+      {
+        user ?
+          <Route path="/" element={<Dashboard user={user} />} />
+        :
+          <Route path="/" element={<Landing />} />
+      }
 
-  return (
-    <>
-      <NavBar user={user} />
-      <h1>Hello world!</h1>
-    </>
-  )
-}
+      <Route path='/signup' element={<SignupForm  setUser={setUser}/>} /> // import the signup form here
+    </Routes>
+  </>
+)}
 
 export default App
-
