8476c882cd12a1ec9fde220d75a17bd3bd68f728
SpiritedVibes <austinrassman@gmail.com>
2024-11-13 14:29:59 -0500

    signin

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar/NavBar'
 import Landing from './components/Landing/Landing'
 import Dashboard from './components/Dashboard/Dashboard'
 import SignupForm from './components/SignupForm/SignupForm' // import the SignupForm
+import SigninForm from './components/SigninForm/SigninForm'
 
 const App = () => {
   const [user, setUser] = useState(null)
@@ -19,8 +20,9 @@ return (
           <Route path="/" element={<Landing />} />
       }
 
-      <Route path='/signup' element={<SignupForm  setUser={setUser}/>} /> // import the signup form here
-    </Routes>
+      <Route path='/signup' element={<SignupForm setUser={setUser}/>} /> // import the signup form here
+      <Route path='/signin' element={<SigninForm setUser={setUser}/>} />
+      </Routes>
   </>
 )}
 
