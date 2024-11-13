d799b9f4a56aea6f2546722f1bdc84073900f7c3
SpiritedVibes <austinrassman@gmail.com>
2024-11-13 14:48:16 -0500

    working on functionality

--- src/services/authService.js
+++ src/services/authService.js
@@ -10,8 +10,14 @@ const signup = async (formData) => {
     if (res.data.error) {
       throw new Error(res.data.error)
     }
-    return res.data
 
+    if (res.data.token) {
+      localStorage.setItem('token', res.data.token)
+
+      const user = JSON.parse(atob(res.data.token.split('.')[1]))
+      return user
+    }
+    return res.data
   } catch (err) {
     console.log(err)
     throw err
@@ -20,23 +26,35 @@ const signup = async (formData) => {
 
 const signin = async (user) => {
   try {
-    const res = await axios.post(`${BACKEND_URL}/users/signin`, user);
+    const res = await axios.post(`${BACKEND_URL}/users/signin`, user)
 
     if (res.data.error) {
-      throw new Error(res.data.error);
+      throw new Error(res.data.error)
     }
 
     if (res.data.token) {
-      const user = JSON.parse(atob(res.data.token.split('.')[1]));
+      localStorage.setItem('token', res.data.token)
+
+      const user = JSON.parse(atob(res.data.token.split('.')[1]))
       return user
     }
+
   } catch (err) {
-    console.log(err);
-    throw err;
+    console.log(err)
+    throw err
   }
+}
+
+const getUser = () =>  {
+  const token = localStorage.getItem('token')
+  if (!token) return null  // if the if statement is only one line you don't need {}
+
+  const user = JSON.parse(atob(token.split('.')[1]))
+  return user
 }
 
 export {
   signup,
-  signin
+  signin,
+  getUser
 }
